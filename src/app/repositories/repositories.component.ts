39e6bcf0498666472ede1544b6a03ef12bd33cde
asder707 <abn.sebick@gmail.com>
2017-02-27 11:25:13 -0300

    directivas de atributos, property binding

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
+  repositories : any = [];
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(()=>{
+      this.repositories = [
+        {name: "Angular curso de abner", description: "Proyecto de prueba que hace el abner"},
+        {name: "Clasificador de dinosaurios", description: "App web para clasificar dinosaurios"},
+        {name: "Mesclador de datos", description: "mesclador de datos web"}
+      ];
+    },2000);
+
+    setTimeout(()=>{
+      this.repositories = [];
+    },5000);
   }
 
 }
